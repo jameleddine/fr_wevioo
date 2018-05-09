c64952540c6d549e6fd6c387fcd91c372ccc858f
Jamel Eddine MEJRI <mejri.jameleddine@gmail.com>
2018-05-09 14:59:29 +0100

    sN°4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+//import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +11,7 @@ import { NavbarComponent } from './home/navbar/navbar.component';
 import { SidebarComponent } from './home/sidebar/sidebar.component';
 import { ContainerComponent } from './home/container/container.component';
 import { MydirectiveDirective } from './mydirective.directive';
+import { MyfilterPipe } from './myfilter.pipe';
 
 
 @NgModule({
@@ -17,11 +21,14 @@ import { MydirectiveDirective } from './mydirective.directive';
     NavbarComponent,
     SidebarComponent,
     ContainerComponent,
-    MydirectiveDirective
+    MydirectiveDirective,
+    MyfilterPipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/myfilter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'myfilter'
+})
+export class MyfilterPipe implements PipeTransform {
+
+  transform(items: any[], term): any {
+    console.log('term', term);     
+    return term ? items.filter(item => item.name.indexOf(term) !== -1): items;
+}
+
+
+}
